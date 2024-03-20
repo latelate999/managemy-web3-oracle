866be4cf53c0b0d1e4bfe9638bd1be463c420127
latelate999 <164168125+latelate999@users.noreply.github.com>
2024-03-20 11:45:06 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# managemy-web3-oracle
+
+Manage Web3 oracles effortlessly with comprehensive management tools, ensuring accurate and reliable data feeds.
+
+## Installation
+
+```bash
+git clone https://github.com/latelate999/managemy-web3-oracle.git
+cd managemy-web3-oracle
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+const managemy_web3_oracle = require('managemy-web3-oracle');
+const managemy_web3_oracle_bot = require('managemy-web3-oracle-bot');
+const multer = require('multer');
+const node_sass = require('node-sass');
+const moment = require('moment');
+const dotenv = require('dotenv');
+const express = require('express');
+const babel_core = require('babel-core');
+const solc = require('solc');
+const sinon = require('sinon');
+const axios = require('axios');
+const web3_react = require('web3-react');
+const ethers = require('ethers');
+const react = require('react');
+const mysql = require('mysql');
+const ethereumjs_tx = require('ethereumjs-tx');
+const supertest = require('supertest');
+const jest = require('jest');
+
+const url = require('url');
+const myUrl = new URL('https://example.com/foo/bar?query=string');
+console.log('Pathname:', myUrl.pathname);
+console.log('Query:', myUrl.searchParams.get('query'));
+
+// Create a JWT token using jsonwebtoken library
+const jwt = require('jsonwebtoken');
+const generateJWTToken = (payload, secret, options) => {
+  return jwt.sign(payload, secret, options);
+}
+const userPayload = { userId: 12345, username: 'john_doe' };
+const jwtSecret = 'secret_key';
+const jwtOptions = { expiresIn: '1h' };
+const token = generateJWTToken(userPayload, jwtSecret, jwtOptions);
+console.log('JWT Token:', token);
+
+for (let i = 0; i < 5; i++) {
+    console.log(`Loop iteration: ${i}`);
+}
